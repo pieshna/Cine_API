1223ff13b28a81ec23355791087c4272bca55424
Jose Miguel Cabrera Murcia <cabreraj525@gmail.com>
2022-08-25 01:44:27 -0600

    First commit

---
+++ src/connection.js
@@ -0,0 +1,16 @@
+//connection with mongodb atlas
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connection = mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bdund.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((err) => {
+    console.log("Error: ", err.message);
+  });
+
+module.exports = connection;
